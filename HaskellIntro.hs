6bbc25ab485b6ee0341fd8701d27dd9f99d95a4f
saquibb08 <saquibb823@gmail.com>
2022-08-11 01:23:11 -0400

    Homework 5

--- HaskellIntro.hs
+++ HaskellIntro.hs
@@ -15,29 +15,39 @@ isThisWorking = "Yes"
 --
 
 lastDigit :: Integer -> Integer
-lastDigit = error "lastDigit not yet defined"
+lastDigit n = n `rem` 10 
 
 dropLastDigit :: Integer -> Integer
-dropLastDigit = error "dropLastDigit not yet defined"
+dropLastDigit n = (n - (lastDigit n)) `div` 10 
 
 toDigits :: Integer -> [Integer]
-toDigits = error "toDigits not yet defined"
+toDigits n | n <= 0       = []
+           | otherwise = (toDigits (dropLastDigit n)) ++ [lastDigit n]
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = error "doubleEveryOther not yet defined"
+doubleEveryOther (x:xs) | null xs = [x] 
+                        | ((length xs)+1) `mod` 2 /= 0 = x : doubleEveryOther xs 
+                        | ((length xs)+1) `mod` 2 == 0 = 2*x : doubleEveryOther xs
 
 sumDigits :: [Integer] -> Integer
-sumDigits = error "sumDigits not yet defined"
+sumDigits [] = 0 
+sumDigits (x:xs) | (dropLastDigit x) > 0 = sumDigits (toDigits x) 
+                 | otherwise = x + (sumDigits xs) 
 
 validate :: Integer -> Bool
-validate = error "validate not yet defined"
-
+validate n | (sumDigits(doubleEveryOther((toDigits n))) `mod` 10 == 0) = True
+           | otherwise = False
 --
 -- Problem 2
 --
 
 pow :: (a -> a) -> Int -> a -> a
-pow = error "pow not yet defined"
+pow f n | n==0 = f
+        | otherwise = powHelper f n f 
+
+powHelper :: (a -> a) -> Int -> (a -> a) -> a -> a
+powHelper f n k | n == 1 = k
+                | otherwise = powHelper f (n-1) (f.k)
 
 g :: Integer -> Integer
 g = error "g not yet defined"
--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Put anything else you want us to know here.
 
 Please let us know how much time you spent on each problem. You may enter time using any format described [here](https://github.com/wroberts/pytimeparse). Please put your time estimates *on the same line* as the problem label, not on the following line.
 
-Problem 1:  
+Problem 1: 2h 
 
-Problem 2:  
+Problem 2: 3h 
 
-Problem 3:  
+Problem 3: 1h 
